fd29baf75de7e57a4cb860ff4a23eb150426e59a
luanpanno <luanpanno@gmail.com>
2020-06-24 17:30:25 -0300

    ecoleta finalizado

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -15,7 +15,14 @@ class PointsController{
             .distinct()
             .select('points.*');
 
-        return res.json(points);
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.1.7:3300/uploads/${point.image}`
+            };
+        });
+
+        return res.json(serializedPoints);
     }
 
     async show(req: Request, res: Response){
@@ -27,21 +34,35 @@ class PointsController{
             return res.status(400).json({ message: 'Point not found' });
         }
 
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.1.7:3300/uploads/${point.image}`
+        }
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return res.json({ point, items });
+        return res.json({ point: serializedPoint, items });
     }
 
     async create(req: Request, res: Response){
-        const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body;
+        const {
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            items
+        } = req.body;
 
         const trx = await knex.transaction();
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: req.file.filename,
             name,
             email,
             whatsapp,
@@ -50,16 +71,19 @@ class PointsController{
             city,
             uf
         }
-
+        
         const insertedIds = await trx('points').insert(point);
 
         const point_id = insertedIds[0]
 
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id
-            }
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id
+                }
         });
 
         await trx('point_items').insert(pointItems);
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = 3300;
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(PORT, () => {
     console.log(`Server on at http://localhost:${PORT}`);
